bb79b62afaad482cd409afa9b828701dae40ce8b
reddy <reddyfede@gmail.com>
2023-08-01 22:40:47 +0200

    implement show functionality for the skills resource

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,19 @@
 module.exports = {
-    index
+    index,
+    getOne : show
 }
 
 const Skills = require('../models/skills')
 
 function index(req,res){
     res.render('skills/index', {title: 'List of Skills', skills: Skills.getAll()})
+}
+
+function show(req,res){
+    const id = req.params.id
+    const contextObject = {
+        title : "Skill Page",
+        skill : Skills.getOne(id)
+    }
+    res.render('skills/show', contextObject)
 }
--- routes/skills.js
+++ routes/skills.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills')
 
 router.get('/', skillsCtrl.index);
+router.get('/:id', skillsCtrl.getOne);
 
 module.exports = router;
